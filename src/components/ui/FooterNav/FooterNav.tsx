6bd78a8c3c23f7b03d3512616da06dec700274a5
Sergey <sk85post@gmail.com>
2024-09-15 15:52:25 +0300

    feat: add burger

--- src/components/ui/FooterNav/FooterNav.tsx
+++ src/components/ui/FooterNav/FooterNav.tsx
@@ -1,20 +1,55 @@
-import { NavLink } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import styles from './FooterNav.module.css';
 
+const toSection = (section: HTMLElement) => {
+  return section?.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+};
+
 const FooterNav = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [targetSection, setTargetSection] = useState<string | null>(null);
+
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id);
+
+    if (section) {
+      toSection(section);
+    } else {
+      setTargetSection(id);
+      if (location.pathname !== '/') {
+        navigate('/');
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (targetSection && location.pathname === '/') {
+      const section = document.getElementById(targetSection);
+      if (section) {
+        toSection(section);
+        setTargetSection(null);
+      }
+    }
+  }, [location.pathname, targetSection]);
+
   return (
     <nav className="nav">
       <ul className={styles['nav-list']}>
         <li className={styles['nav-item']}>
-          <NavLink to="#" aria-label="catalog">
+          <div onClick={() => scrollToSection('catalog')} aria-label="catalog">
             Catalog
-          </NavLink>
+          </div>
         </li>
         <li className={styles['nav-item']}>
-          <NavLink to="#" aria-label="faq">
+          <div onClick={() => scrollToSection('faq')} aria-label="faq">
             FAQ
-          </NavLink>
+          </div>
         </li>
       </ul>
     </nav>
--- src/components/ui/HeaderNav/HeaderNav.tsx
+++ src/components/ui/HeaderNav/HeaderNav.tsx
@@ -1,21 +1,60 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import styles from './HeaderNav.module.css';
 import CartIcon from '../../icons/CartIcon/CartIcon';
+import Burger from '../Burger/Burger';
+
+const toSection = (section: HTMLElement) => {
+  return section?.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+};
 
 const HeaderNav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [targetSection, setTargetSection] = useState<string | null>(null);
+
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id);
+
+    if (section) {
+      toSection(section);
+    } else {
+      setTargetSection(id);
+      if (location.pathname !== '/') {
+        navigate('/');
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (targetSection && location.pathname === '/') {
+      const section = document.getElementById(targetSection);
+      if (section) {
+        toSection(section);
+        setTargetSection(null);
+      }
+    }
+    setMenuOpen(false);
+  }, [location.pathname, targetSection]);
+
   return (
     <nav className="nav">
-      <ul className={styles['nav-list']}>
+      <Burger onClick={() => setMenuOpen(!menuOpen)} isOpen={menuOpen} />
+      <ul className={`${styles['nav-list']} ${menuOpen ? styles.active : ''}`}>
         <li className={styles['nav-item']}>
-          <NavLink to="/" aria-label="catalog">
+          <div onClick={() => scrollToSection('catalog')} aria-label="catalog">
             Catalog
-          </NavLink>
+          </div>
         </li>
         <li className={styles['nav-item']}>
-          <NavLink to="/" aria-label="faq">
+          <div onClick={() => scrollToSection('faq')} aria-label="faq">
             FAQ
-          </NavLink>
+          </div>
         </li>
         <li className={styles['nav-item']}>
           <NavLink to="/cart" aria-label="cart">
